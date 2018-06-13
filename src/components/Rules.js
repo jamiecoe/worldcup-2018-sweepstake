56ef0126d478d509e6f7c4e2b3766a58dc4d95aa
Jamie Coe <jamiecoe66@gmail.com>
2018-06-13 09:03:17 +0100

    refactor: change text for rules

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -5,8 +5,8 @@ const Rules = () => {
     <div>
       <h2>The rules:</h2>
       <ul>
-        <li>Each person gets <strong>2 top</strong>, <strong>2 mid</strong> and <strong>4 low</strong> countries (country rankings are available <a href="http://www.fifa.com/fifa-world-ranking/ranking-table/men/index.html" rel="noopener noreferrer" target="_blank">here</a>)</li>
-        <li>Whoever has the country that wins the World Cup will win the sweep stake</li>
+        <li>Each player gets <strong>2 top</strong>, <strong>2 mid</strong> and <strong>4 low</strong> countries (country rankings are available <a href="http://www.fifa.com/fifa-world-ranking/ranking-table/men/index.html" rel="noopener noreferrer" target="_blank">here</a>)</li>
+        <li>The player with the country that wins the World Cup, wins the sweep stake</li>
       </ul>
     </div>  
   )
