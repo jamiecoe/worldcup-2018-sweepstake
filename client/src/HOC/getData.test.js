be75be128aedc25058f3415cefb415ecc1ecfc49
Jamie Coe <jamiecoe66@gmail.com>
2018-07-05 18:22:06 +0100

    test(client): add test for getData HOC

--- client/src/HOC/getData.test.js
+++ client/src/HOC/getData.test.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from '..Components/App';
+import App from '../Components/App';
+import getData from './getData';
 
 describe('getData HOC', () => {
-  
+  it('should produce a component that renders properly', () => {
+    const appWithData = getData(App);
+    const appWithDataRendered = shallow(<appWithData />);
+
+    expect(appWithDataRendered).toMatchSnapshot();
+  })
 })
