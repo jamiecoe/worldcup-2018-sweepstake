f64b2a83a05152941cb1f91a22e3527d3454aec5
Jamie Coe <jamiecoe66@gmail.com>
2018-07-14 10:36:28 +0100

    refactor(client): modularise global styling

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,41 +1,11 @@
 import React from 'react';
-import styled, { injectGlobal } from 'styled-components';
+import styled from 'styled-components';
 
 import Header from './Header';
 import Countdown from './Countdown';
 import Rules from './Rules';
 import CountryOptions from './CountryOptions/CountryOptions';
 import PlayerList from './Players/PlayerList';
-
-import dusha from '../assets/fonts/dusha.ttf';
-
-injectGlobal`
-  @font-face {
-    font-family: 'dusha';
-    src: url(${dusha});
-  }
-
-  h1, h2, h3 {
-    font-family: dusha;
-  }
-
-  h1 {
-    font-size: 4rem;
-  }
-
-  p, a, li {
-    font-family: 'Open Sans', sans-serif;
-  }
-
-  a {
-    color: gold;
-  }
-
-  body {
-    background: #0f4583 url("https://www.fifa.com/assets/img/tournaments/17/2018/common/fwc_darkbluebg.png") repeat;
-    color: white;
-  }
-`;
 
 const CenteredDiv = styled.div`
   display: flex;
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './Components/App';
 import getData from './HOC/getData';
 import registerServiceWorker from './registerServiceWorker';
+import './styles/globalStyling'
 
 const AppWithData = getData(App);
 
