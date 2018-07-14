f7534d999add84820458c2f7587cf01c6282e0b0
Jamie Coe <jamiecoe66@gmail.com>
2018-07-14 10:16:28 +0100

    fix(client): remove gidi's setTimeout 😑

--- client/src/HOC/getData.js
+++ client/src/HOC/getData.js
@@ -1,41 +1,39 @@
-import React, { Component } from 'react';
-import firebase from '../utils/firebase';
-import { isEmpty } from 'lodash';
+import React, { Component } from 'react'
+import firebase from '../utils/firebase'
+import { isEmpty } from 'lodash'
 
 const getData = WrappedComponent => {
   return class extends Component {
     constructor() {
-      super();
-      this.state = {};
+      super()
+      this.state = {}
     }
 
     componentDidMount() {
-      this.getData();
+      this.getData()
     }
 
     getData = () => {
-      const dbRef = firebase.database().ref();
+      const dbRef = firebase.database().ref()
       dbRef.on('value', snapshot => {
-        const { countries, players } = snapshot.val();
-        setTimeout(() => {
-          this.setState({
-            countries,
-            players
-          });
-        }, 5000);
-      });
-    };
+        const { countries, players } = snapshot.val()
+        this.setState({
+          countries,
+          players
+        });
+      })
+    }
 
     render() {
       if (isEmpty(this.state)) {
         return <span>Loading...</span>
       }
 
-      const { countries, players } = this.state;
+      const { countries, players } = this.state
 
-      return <WrappedComponent countries={countries} players={players} />;
+      return <WrappedComponent countries={countries} players={players} />
     }
-  };
-};
+  }
+}
 
-export default getData;
+export default getData
