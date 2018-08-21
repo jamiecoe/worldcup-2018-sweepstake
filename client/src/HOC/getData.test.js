08fc62d7405f06493d98a4d04fdce3de7f8454bc
Jamie Coe <jamiecoe66@gmail.com>
2018-08-21 11:23:37 +0100

    test(client): remove snapshots from getData tests

--- client/src/HOC/getData.test.js
+++ client/src/HOC/getData.test.js
@@ -1,13 +1,55 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import App from '../Components/App';
-import getData from './getData';
+import React from 'react'
+import { shallow } from 'enzyme'
+import getData from './getData'
 
 describe('getData HOC', () => {
-  it('should produce a component that renders properly', () => {
-    const appWithData = getData(App);
-    const appWithDataRendered = shallow(<appWithData />);
+  it('should initially show the loading <span>, when state is empty', () => {
+    const MockComponent = () => <div>Test Component</div>
+    const mockGetFirebaseData = () => Promise.resolve({})
+    
+    const MockComponentWithData = getData(MockComponent, mockGetFirebaseData)
+    const mockComponentWithData = shallow(<MockComponentWithData />)
 
-    expect(appWithDataRendered).toMatchSnapshot();
+    expect(mockComponentWithData.contains(<span>Loading...</span>)).toBe(true)
   })
+
+  it('should initially show the loading <span>, when state is empty', () => {
+    const MockComponent = () => <div>Test Component</div>
+    const mockGetFirebaseData = jest.fn(() =>
+      Promise.resolve({
+        countries: {},
+        players: {}
+      })
+    )
+    const MockComponentWithData = getData(MockComponent, mockGetFirebaseData)
+    const mockComponentWithData = shallow(<MockComponentWithData />)
+
+    expect(mockComponentWithData.contains(<span>Loading...</span>)).toBe(true)
+  })
+
+  // it('should produce a new component that wraps around MockComponent', () => {
+  //   const MockComponent = () => <div>Test Component</div>
+  //   const mockVal = jest.fn().mockReturnValue({
+  //     countries: {},
+  //     players: {}
+  //   })
+  //   const mockOn = jest.fn(() =>
+  //     Promise.resolve({
+  //       val: mockVal
+  //     })
+  //   )
+
+  //   const mockDbRef = {
+  //     on: mockOn
+  //   }
+
+  //   const MockComponentWithData = getData(MockComponent, mockDbRef)
+  //   const mockComponentWithData = shallow(<MockComponentWithData />)
+
+  //   expect(mockVal).toHaveBeenCalled()
+  //   console.log(mockComponentWithData.contains(<div>Test Component</div>))
+  //   console.log(mockComponentWithData.state())
+  // })
+
+  it('produces a component which calls getUser() method when the component mounts', () => {})
 })
