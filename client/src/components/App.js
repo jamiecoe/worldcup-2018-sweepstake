65cc417893fdf9530f42144ee69edbc8e3ac70f2
jamiecoe <jamiecoe66@gmail.com>
2018-10-22 17:25:52 +0100

    style(client): remove unnecessary semi-colons

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from 'react'
+import styled from 'styled-components'
 
-import Header from './Header';
-import Countdown from './Countdown';
-import Rules from './Rules';
-import CountryOptions from './CountryOptions/CountryOptions';
-import PlayerList from './Players/PlayerList';
+import Header from './Header'
+import Countdown from './Countdown'
+import Rules from './Rules'
+import CountryOptions from './CountryOptions/CountryOptions'
+import PlayerList from './Players/PlayerList'
 
 const CenteredDiv = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-`;
+`
 
 const App = props => {
-  const { countries, players } = props;
-  return (
-    <CenteredDiv>
-      <Header />
-      {/* <Countdown /> */}
-      <PlayerList players={players} countries={countries} />
-      <CountryOptions countries={countries} />
-      {/* <Rules /> */}
-    </CenteredDiv>
-  );
-};
+    const { countries, players } = props
+    return (
+        <CenteredDiv>
+            <Header/>
+            {/* <Countdown /> */}
+            <PlayerList players={players} countries={countries}/>
+            <CountryOptions countries={countries}/>
+            {/* <Rules /> */}
+        </CenteredDiv>
+    )
+}
 
-export default App;
+export default App
